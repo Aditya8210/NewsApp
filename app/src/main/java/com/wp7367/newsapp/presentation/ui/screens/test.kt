cb44585b59487d4dfcb191a7b241bd609e5f703b
Aditya <adityahp9097@gmail.com>
2026-05-09 22:46:42 +0530

    2 test of branches

--- app/src/main/java/com/wp7367/newsapp/presentation/ui/screens/test.kt
+++ app/src/main/java/com/wp7367/newsapp/presentation/ui/screens/test.kt
@@ -1,9 +1,12 @@
 package com.wp7367.newsapp.presentation.ui.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Test(modifier: Modifier = Modifier) {
+
+    Text(text = "hello")
 
 }
